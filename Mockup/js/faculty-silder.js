e00d7dcdc92ebb7de6fe961cc45aafb9d6430696
Quách Thanh Phong <dungdm62@wru.vn>
2021-08-13 22:17:15 +0700

    no message

---
+++ Mockup/js/faculty-silder.js
@@ -0,0 +1,19 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    nav:false,
+    dots:true,
+    items:4,
+    responsive:{
+        0:{
+            items:1
+        },
+        720:{
+            items:2
+        },
+        960:{
+            items:4
+        }
+    }
+})
---
+++ Mockup/js/rating-slider.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    nav:false,
+    dots:false,
+    items:4,
+    responsive:{
+        0:{
+            items:1
+        },
+        720:{
+            items:2
+        }
+    }
+})
